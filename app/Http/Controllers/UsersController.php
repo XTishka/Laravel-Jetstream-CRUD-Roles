cb2697f8cf63103a44b0cda285293e6d323db27a
XTishka <takhir.berdyiev@gmail.com>
2021-03-06 06:54:17 +0200

    FIX :: Исправляет обновление пароля при редактировании пользователя

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,66 +16,56 @@ class UsersController extends Controller
     public function index()
     {
         abort_if(Gate::denies('user_management'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         $users = User::with('roles')->get();
-
         return view('users.index', compact('users'));
     }
 
     public function create()
     {
         abort_if(Gate::denies('user_management'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         $roles = Role::pluck('title', 'id');
-
         return view('users.create', compact('roles'));
     }
 
     public function store(StoreUserRequest $request)
     {
-
-        $request->merge([
-            'password' => Hash::make($request->input('password'))
-        ]);
-
+        $request->merge(['password' => Hash::make($request->input('password'))]);
         $user = User::create($request->all());
         $user->roles()->sync($request->input('roles', []));
-
         return redirect()->route('users.index');
     }
 
     public function show(User $user)
     {
         abort_if(Gate::denies('user_management'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         return view('users.show', compact('user'));
     }
 
     public function edit(User $user)
     {
         abort_if(Gate::denies('user_management'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         $roles = Role::pluck('title', 'id');
-
         $user->load('roles');
-
         return view('users.edit', compact('user', 'roles'));
     }
 
     public function update(UpdateUserRequest $request, User $user)
     {
-        $user->update($request->validated());
+        $password = $request->input('password');
+        if ( $password != null) {
+            $request->merge(['password' => Hash::make($password)]);
+            $user->update($request->all());
+        } else {
+            $user->update($request->validated());
+        }
         $user->roles()->sync($request->input('roles', []));
-
         return redirect()->route('users.index');
     }
 
     public function destroy(User $user)
     {
         abort_if(Gate::denies('user_management'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         $user->delete();
-
         return redirect()->route('users.index');
     }
 }
