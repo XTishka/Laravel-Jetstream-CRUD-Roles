8a20763a158f18741b2b41686aecb449f6cb527a
Takhir Berdyiev <takhir.berdyiev@gmail.com>
2021-04-12 07:44:47 +0300

    PRETTIFY :: Finance/Currency :: Добавляет SVG иконки

--- Modules/Finance/Resources/views/currencies/index.blade.php
+++ Modules/Finance/Resources/views/currencies/index.blade.php
@@ -9,11 +9,17 @@
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8 flex justify-between">
                 <a href="{{ route('finance.currencies.create') }}"
-                   class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                   class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex">
+                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
                     @lang('finance::currencies.add_currency')
                 </a>
                 <a href="{{ route('finance.currencies.trash') }}"
-                   class="bg-gray-400 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded">
+                   class="bg-gray-400 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded flex" title="Go to trash items">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg>
                     @lang('finance::currencies.trash')
                 </a>
             </div>
@@ -68,22 +74,30 @@
                                             {{ $currency->designation }}
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium flex justify-end">
                                             <a href="{{ route('finance.currencies.show', $currency->id) }}"
-                                               class="text-blue-600 hover:text-blue-900 mb-2 mr-2">
-                                                @lang('finance::currencies.view')
+                                               class="text-blue-600 hover:text-blue-900 mb-2 mr-2" title="@lang('finance::currencies.view')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
+                                                </svg>
                                             </a>
                                             <a href="{{ route('finance.currencies.edit', $currency->id) }}"
-                                               class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">
-                                                @lang('finance::currencies.edit')
+                                               class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2" title="@lang('finance::currencies.edit')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
                                             </a>
                                             <form class="inline-block"
                                                   action="{{ route('finance.currencies.destroy', $currency->id) }}"
                                                   method="POST" onsubmit="return confirm(@lang('finance::currencies.sure'));">
                                                 <input type="hidden" name="_method" value="DELETE">
                                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                <input type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2"
-                                                       value="@lang('finance::currencies.delete')">
+                                                <button type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2"
+                                                       value="@lang('finance::currencies.delete')" title="@lang('finance::currencies.delete')">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                    </svg>
+                                                </button>
                                             </form>
                                         </td>
                                     </tr>
--- Modules/Finance/Resources/views/currencies/trash.blade.php
+++ Modules/Finance/Resources/views/currencies/trash.blade.php
@@ -8,7 +8,10 @@
     <div>
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8">
-                <a href="{{ route('finance.currencies') }}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
+                <a href="{{ route('finance.currencies') }}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded inline-flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+                    </svg>
                     @lang('finance::currencies.back_to_list')
                 </a>
             </div>
@@ -63,24 +66,30 @@
                                             {{ $currency->designation }}
                                         </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-{{--                                            <a href="{{ route('finance.currencies.restore', $currency->id) }}"--}}
-{{--                                               class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">Restore</a>--}}
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium flex justify-end">
                                             <form class="inline-block"
                                                   action="{{ route('finance.currencies.restore', $currency->id) }}"
                                                   method="POST" onsubmit="return confirm(@lang('finance::currencies.sure'));">
                                                 <input type="hidden" name="_method" value="PUT">
                                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                <input type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2"
+                                                <button type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2"
                                                        value="@lang('finance::currencies.restore')">
+                                                       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-600 hover:text-indigo-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z" />
+                                                        </svg>
+                                                </button>
                                             </form>
                                             <form class="inline-block"
                                                   action="{{ route('finance.currencies.forcedelete', $currency->id) }}"
                                                   method="POST" onsubmit="return confirm(@lang('finance::currencies.sure'));">
                                                 <input type="hidden" name="_method" value="DELETE">
                                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                <input type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2"
+                                                <button type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2"
                                                        value="@lang('finance::currencies.complete_delete')">
+                                                       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                    </svg>
+                                                </button>
                                             </form>
                                         </td>
                                     </tr>
@@ -93,7 +102,10 @@
             </div>
 
             <div class="block mb-8 mt-10">
-                <a href="{{ route('finance.currencies') }}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
+                <a href="{{ route('finance.currencies') }}" class="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded inline-flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+                    </svg>
                     @lang('finance::currencies.back_to_list')
                 </a>
             </div>
